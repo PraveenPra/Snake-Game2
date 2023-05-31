d07952dfc9863dc3b27ba83c107868a935259f0b
manikanta500 <manikanta@tharsha.com>
2023-05-31 13:10:38 +0530

    Made the body parts move and turn as the head turns and moves

--- Snake 2/Assets/Code/SnakeController.cs
+++ Snake 2/Assets/Code/SnakeController.cs
@@ -8,7 +8,7 @@ public class SnakeController : MonoBehaviour
     public float steerSpeed = 100;
     public GameObject BodyPrefab;
     private int gap = 30;
-
+    private float bodySpeed = 3;
     private List<GameObject> BodyParts = new List<GameObject>();
     private List<Vector3> HeadPositionHistory = new List<Vector3>();
     // Start is called before the first frame update
@@ -24,24 +24,31 @@ public class SnakeController : MonoBehaviour
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
         //steer
-       float steerDir = Input.GetAxis("Horizontal");
+        float steerDir = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * steerDir * steerSpeed * Time.deltaTime);
 
         //store snake head positions
-        HeadPositionHistory.Insert(0,transform.position);
+        HeadPositionHistory.Insert(0, transform.position);
 
         //move the each body parts to that headposition list
-       int index = 0;
-        foreach(var body in BodyParts){
-            Vector3 point = HeadPositionHistory[Mathf.Min(index + gap,HeadPositionHistory.Count -1)];
+        int index = 0;
+        foreach (var body in BodyParts)
+        {
+            Vector3 point = HeadPositionHistory[Mathf.Min(index * gap, HeadPositionHistory.Count - 1)];
 
-            body.transform.position = point;
+            Vector3 headDirection = point - body.transform.position;
+
+            body.transform.position += headDirection * bodySpeed * Time.deltaTime;
+
+            body.transform.LookAt(point);
+
             index++;
 
         }
     }
 
-    private void GrowSnake(){
+    private void GrowSnake()
+    {
         GameObject body = Instantiate(BodyPrefab);
         BodyParts.Add(body);
     }
